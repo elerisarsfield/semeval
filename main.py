6651afc2f603ce1dd8d73f92016b1a1bd18a6e5a
Eleri Sarsfield <elerisarsfield@gmail.com>
2020-02-25 10:21:19 +0000

    add in separate inference for semeval task 2

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ parser.add_argument(
     'start_corpus', type=str, help='address of the older (reference) corpus')
 parser.add_argument('end_corpus', type=str,
                     help='address of the newer (focus) corpus')
-parser.add_argument('--semeval_mode', type=bool, help='True if the project is being used for SemEval 2020 Task 1, False if the project is being used for general inference, default False', default=False, metavar='M')
-parser.add_argument('targets', type=str, help='address of the target words', nargs='?')
+parser.add_argument('--semeval_mode', type=bool,
+                    help='True if the project is being used for SemEval 2020 Task 1, False if the project is being used for general inference, default False', default=False, metavar='M')
+parser.add_argument('targets', type=str,
+                    help='address of the target words', nargs='?')
 parser.add_argument('output', type=str, help='address to write output to')
 parser.add_argument('--max_iters', type=int, metavar='N', default=25,
                     help='maximum number of iterations to run sampling for')
@@ -27,10 +29,13 @@ parser.add_argument('--window_size', metavar='W', type=int, default=10,
                     help='size of context window to use, default 10')
 parser.add_argument('--floor', type=int, metavar='F', default=1,
                     help='minimum number of occurrences to be considered, default 1')
+parser.add_argument('--threshold', type=float, metavar='T', default=0.6,
+                    help='minimum score for novelty_diff before a word is considered to have a novel sense')
 
 args = parser.parse_args()
 if args.semeval_mode and 'targets' not in vars(args):
     parser.error('targets arg is required when in SemEval mode')
+
 
 def main():
     start_time = time.time()
@@ -49,21 +54,26 @@ def main():
     save_path = os.path.join(args.output, 'saves')
     if not os.path.exists(save_path):
         os.makedirs(save_path)
+    if not os.path.exists(os.path.join(args.output, 'task1')):
+        os.makedirs(os.path.join(args.output, 'task1'))
+    if not os.path.exists(os.path.join(args.output, 'task2')):
+        os.makedirs(os.path.join(args.output, 'task2'))
 
     print('Loading words...')
-    corpus = Corpus(args.start_corpus, save_path, args.end_corpus, args.floor, args.window_size)
+    corpus = Corpus(args.start_corpus, save_path,
+                    args.end_corpus, args.floor, args.window_size)
     print('Setting up initial partition...')
     for i in corpus.docs:
         i.init_partition(args.alpha)
 
-    hdp = HDP(corpus.vocab_size, save_path, eta=args.eta, alpha=args.alpha, gamma=args.gamma)
+    hdp = HDP(corpus.vocab_size, save_path, eta=args.eta,
+              alpha=args.alpha, gamma=args.gamma)
     hdp.init_partition(corpus.docs)
     for i in corpus.docs:
         i.topic_to_distribution(hdp.senses.shape[0])
 
     print('Done')
     it = 0
-    stopping = 1.0
     print(f'Running Gibbs sampling for {args.max_iters} iterations...')
     while it < args.max_iters:
         it += 1
@@ -95,17 +105,28 @@ def main():
                     else:
                         word.senses[sense][1] += 1
                     words[word.word] = word
-
     for k, v in words.items():
-        v = v.calculate()
-    assert max(words.values()) <= 1
-    assert min(words.values()) >= 0
+        words[k] = v.calculate()
     if args.semeval_mode:
-        print(f'Running separate inference on the two corpora...')
         targets = utils.get_targets(args.targets)
-        corpus1 = Corpus(args.start_corpus, save_path, floor=args.floor, window_size=args.window_size)
-        corpus2 = Corpus(args.start_corpus, save_path, floor=args.floor, window_size=args.window_size)
-        hdp = HDP(corpus1.vocab_size, save_path, eta=args.eta, alpha=args.alpha, gamma=args.gamma)
+        with open(os.path.join(os.path.join(args.output, 'task1'),
+                               'english.txt'), 'w') as f:
+            for i in range(len(targets)):
+                t = targets[i][0]
+                pos = targets[i][1]
+                recombine = t+'_'+pos
+                score = words[t][0]
+                different = 1 if score > args.threshold else 0
+                f.write(f'{recombine} {different}\n')
+
+        print(f'Running separate inference on the two corpora...')
+
+        corpus1 = Corpus(args.start_corpus, save_path,
+                         floor=args.floor, window_size=args.window_size)
+        corpus2 = Corpus(args.start_corpus, save_path,
+                         floor=args.floor, window_size=args.window_size)
+        hdp = HDP(corpus1.vocab_size, save_path, eta=args.eta,
+                  alpha=args.alpha, gamma=args.gamma)
         hdp.init_partition(corpus1.docs)
         it = 0
         while it < args.max_iters:
@@ -116,7 +137,8 @@ def main():
         for i in hdp.senses:
             i /= i.sum()
         dist_1 = hdp.senses
-        hdp = HDP(corpus2.vocab_size, save_path, eta=args.eta, alpha=args.alpha, gamma=args.gamma)
+        hdp = HDP(corpus2.vocab_size, save_path, eta=args.eta,
+                  alpha=args.alpha, gamma=args.gamma)
         hdp.init_partition(corpus2.docs)
         it = 0
         while it < args.max_iters:
@@ -127,13 +149,16 @@ def main():
         for i in hdp.senses:
             i /= i.sum()
         dist_2 = hdp.senses
-        for i in targets:
-            index = (corpus1.word_to_idx[i], corpus2.word_to_idx[i])
-            jensenshannon = dist.jensenshannon(dist1[index[0]], dist2[index[1]])
+        with open(os.path.join(os.path.join(args.output, 'task2'), 'english.txt'), 'w') as f:
+            for i in range(len(targets)):
+                t = targets[i][0]
+                pos = targets[i][1]
+                recombine = t+'_'+pos
+                index = (corpus1.word_to_idx[t], corpus2.word_to_idx[t])
+                jensenshannon = dist.jensenshannon(
+                    dist_1[index[0]], dist_2[index[1]])
+                f.write(f'{recombine} {jensenshannon}\n')
         print('Done.')
-        
-    for i in corpus.docs:
-        i.topic_to_distribution(hdp.senses.shape[0])
 
     else:
         top_k = 50
